eef71dffaa4bcdb2387247f8300f7c997b7f9820
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-14 09:25:19 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,31 +2,30 @@ package ru.job4j.oop;
 
 public class Cat {
     private String food;
-
     private String name;
 
     public void giveNick(String nick) {
-            this.name = nick;
+        this.name = nick;
     }
 
-        public void show() {
-            System.out.println("Eда котика: " + this.food + " " + "Имя Котика: "  + this.name);
-        }
+    public void show() {
+        System.out.println("Eда котика: " + this.food + " " + "Имя Котика: "  + this.name);
+    }
 
     public void eat(String meat) {
         this.food = meat;
     }
 
-        public static void main(String[] args) {
-            System.out.println("There are gav's food.");
-            Cat gav = new Cat();
-            gav.giveNick("Gav");
-            gav.eat("kotleta");
-            gav.show();
-            System.out.println("There are black's food.");
-            Cat black = new Cat();
-            black.giveNick("Gav");
-            black.eat("fish");
-            black.show();
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Gav");
+        black.eat("fish");
+        black.show();
     }
 }
