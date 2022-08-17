666ef9f9070ae812d0b1442d85bb7d2687db09c2
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-17 20:52:34 +0300

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         HtmlReport report = new HtmlReport();
-        String text = report.generate("Report's name", "Report's body");
+        String text = report.generate("Report name", "Report's body");
         System.out.println(text);
     }
 }
