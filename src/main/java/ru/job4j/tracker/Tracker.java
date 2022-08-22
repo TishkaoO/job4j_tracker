0819aff1cec8f366a2bdb8ffbf489995f926f28e
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-22 23:00:39 +0300

    8. Что такое валидация? [#246864 #334926]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -46,7 +46,7 @@ public class Tracker {
     private int indexOf(int id) {
         int rsl = -1;
         for (int i = 0; i < size; i++) {
-            if (items[i].getId() == id)     {
+            if (items[i].getId() == id) {
                 rsl = i;
                 break;
             }
@@ -56,12 +56,13 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
-            return false;
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
         }
-        System.arraycopy(items, index + 1, items, index, items.length - index - 1);
-        items[size - 1] = null;
-        size--;
-        return true;
+        return rsl;
     }
 }
+
