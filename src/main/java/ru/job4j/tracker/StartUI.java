b6e69b765ca725eafb4682d2532ec59665a45d49
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-23 19:43:32 +0300

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745] переделал логическую последовательность кода в методе init

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,22 +16,19 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
-            } else if (select == 6) {
-                run = false;
+                System.out.println("Добавленная заявка: " + item);
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
-                for (Item item : items) {
-                    System.out.println(item);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
                 }
-            }
-            Item[] items = tracker.findAll();
-            if (items.length > 0) {
-                for (Item item : items) {
-                    System.out.println(item);
-                }
-            } else {
-                System.out.println("Хранилище еще не содержит заявок");
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
