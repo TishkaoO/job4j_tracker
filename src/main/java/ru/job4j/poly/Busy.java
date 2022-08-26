61e03ed5a407d379ae9172172485cf200f901066
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-26 19:33:29 +0300

    3. Интерфейс Input [#181092 #336058]

--- src/main/java/ru/job4j/poly/Busy.java
+++ src/main/java/ru/job4j/poly/Busy.java
@@ -3,16 +3,25 @@ package ru.job4j.poly;
 public class Busy implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("едет по дороге");
     }
 
     @Override
     public void human(int amount) {
-
+        int people = 4;
+        System.out.println("Количество человек в машине " + people);
     }
 
     @Override
     public int refuel(int fuel, int money) {
+        int petrol = 55;
+        int buy = 50;
+        if (petrol < 10) {
+            System.out.println("покупаем бензин и заправляемся");
+            int purchase = buy * 20;
+        } else {
+            System.out.println("Едем дальше");
+        }
         return money;
     }
 }
