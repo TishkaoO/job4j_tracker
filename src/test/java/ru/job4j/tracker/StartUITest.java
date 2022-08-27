bd7cd63e6fe16880bcb5e8d3cf79231164e4d313
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-27 11:28:20 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #336743]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,6 +16,20 @@ public class StartUITest {
     }
 
     @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = new Item(1, "new item");
@@ -23,6 +37,6 @@ public class StartUITest {
         String[] answers = {Integer.toString(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item del = tracker.findById(Integer.parseInt(answers[0]));
-        assertThat(del);
+        assertThat(del).isNull();
     }
 }
