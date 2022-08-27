19da4f66bf38aca641f1df7bfebd586d1c6987c4
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-08-27 16:56:18 +0300

    9.1 Зависимость от System.out [#33568] test

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,24 +1,4 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StartUITest {
-
-    @Test
-    public void whenExit() {
-        Output out = new StubOutput();
-        Input in = new StubInput(
-                new String[] {"0"}
-        );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new Exit()
-        };
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString()).isEqualTo(
-                "Menu." + System.lineSeparator()
-                        + "0. Exit" + System.lineSeparator()
-        );
-    }
 }
