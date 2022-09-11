f354874a4eb6509abef29a0bb91dd92bd09b0803
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-09-11 10:52:11 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039 #341841]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -38,7 +38,7 @@ public class TrackerTest {
         tracker.add(new Item("Second"));
         tracker.add(new Item("First"));
         List<Item> result = tracker.findByName(first.getName());
-        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.size()).isEqualTo(5);
     }
 
     @Test
