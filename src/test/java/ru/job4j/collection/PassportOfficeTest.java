3b72caf71854bf9078d4750498a30e8f89c00821
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-09-11 12:47:08 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+
+    @Test
+    public void add2() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Vova Sobol");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean result = office.add(citizen2);
+        assertFalse(result);
+    }
+}
