b35d20ce71f17ab7ecc131151dc92a8657e37a87
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-09-14 20:34:29 +0300

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/collection/SortByNameJob.java
+++ src/main/java/ru/job4j/collection/SortByNameJob.java
@@ -2,9 +2,9 @@ package ru.job4j.collection;
 
 import java.util.Comparator;
 
-public class SortDescByNameJob implements Comparator<Job> {
+public class SortByNameJob implements Comparator<Job> {
     @Override
     public int compare(Job left, Job right) {
-        return right.getName().compareTo(left.getName());
+        return left.getName().compareTo(right.getName());
     }
 }
