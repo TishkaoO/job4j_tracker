128f0b155d4f647d092676d6acb77d3feab61d94
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-09-14 21:25:00 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemSortByNameTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ItemSortByNameTest {
+    @Test
+    public void whenItemAscByName() {
+        List<Item> items = Arrays.asList(
+                new Item("Vova"),
+                new Item("Kirill"),
+                new Item("Semen"));
+        List<Item> expected = Arrays.asList(
+                new Item("Kirill"),
+                new Item("Semen"),
+                new Item("Vova"));
+        items.sort(new ItemAscByName());
+        assertEquals(expected, items);
+
+    }
+
+    @Test
+    public void whenItemDescByName() {
+        List<Item> items = Arrays.asList(
+                new Item("Vladimir"),
+                new Item("Vova"),
+                new Item("Kirill"));
+        items.sort(new ItemDescByName());
+        List<Item> expected = List.of(
+                new Item("Kirill"),
+                new Item("Vladimir"),
+                new Item("Vova"));
+        items.sort(new ItemAscByName());
+        assertEquals(expected, items);
+    }
+}
