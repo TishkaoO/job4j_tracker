afe0529136c9cc42efb42096c732d8194be1b4b7
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-10-03 20:00:36 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] s1 = o1.split("/");
+        String[] s2 = o2.split("/");
+        int comp = s2[0].compareTo(s1[0]);
+        return comp == 0 ? o1.compareTo(o2) : comp;
+    }
+}
