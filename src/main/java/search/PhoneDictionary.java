396ae772859641466bf6b6d6be5321c85b3600f4
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-10-09 09:11:32 +0300

    6. Функции высшего порядка   [#24255]

--- src/main/java/search/PhoneDictionary.java
+++ src/main/java/search/PhoneDictionary.java
@@ -1,29 +1,33 @@
 package search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-        private ArrayList<Person> persons = new ArrayList<>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
-        public void add(Person person) {
-            this.persons.add(person);
-        }
+    public void add(Person person) {
+        this.persons.add(person);
+    }
 
-        /**
-         * Вернуть список всех пользователей, который содержат key в любых полях.
-         * @param key Ключ поиска.
-         * @return Список пользователей, которые прошли проверку.
-         */
-        public ArrayList<Person> find(String key) {
-            ArrayList<Person> result = new ArrayList<>();
-            for (Person person : persons) {
-                if (person.getName().contains(key)
-                        || person.getSurname().contains(key)
-                        || person.getPhone().contains(key)
-                        || person.getAddress().contains(key)) {
-                    result.add(person);
-                }
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список пользователей, которые прошли проверку.
+     */
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> combineName = s -> s.getName().contains(key);
+        Predicate<Person> combineSurname = k -> k.getSurname().contains(key);
+        Predicate<Person> combinePhone = y -> y.getPhone().contains(key);
+        Predicate<Person> combineAddress = z -> z.getAddress().contains(key);
+        Predicate<Person> combine = combineName.or(combineSurname).or(combinePhone).or(combineAddress);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
-            return result;
+        }
+        return result;
     }
 }
