291a584369ffcb44322079a3fd3a556c682797d8
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-02-06 22:36:39 +0300

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,15 +1,9 @@
 package ru.job4j.tracker;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 @Data
-@Getter
-@Setter
-@EqualsAndHashCode
 public class Item {
     private LocalDateTime created = LocalDateTime.now();
     private int id;
